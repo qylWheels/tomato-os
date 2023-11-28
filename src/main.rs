47f097e67de167f02f83490111780aab20e8daca
qylWheels <command1748165360@126.com>
2023-11-28 18:49:12 +0800

    Finish chapter2.

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,17 @@
 
 use core::panic::PanicInfo;
 
+static STR: &[u8] = b"Hello kernel world!";
+
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    let vga_buffer = 0xb8000 as *mut u8;
+    for (i, &c) in STR.iter().enumerate() {
+        unsafe {
+            *(vga_buffer.add(i * 2)) = c;
+            *(vga_buffer.add(i * 2 + 1)) = 0x8c;    // bright red
+        }
+    }
     loop {}
 }
 
